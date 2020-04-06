94e46211c16550bfd0dca0e8b4b6cfa093e1319a
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2020-04-06 08:28:20 +0430

    Bot class added. version 0.0.0

---
+++ Bot.java
@@ -0,0 +1,28 @@
+
+
+
+/**
+ * This class is a simple bot that play insted player 2
+ * 
+ * 
+ * @author Mohammad Mahdi Malmasi
+ * @version 0.0.0
+ */
+public class Bot extends Player
+{
+
+
+    /**
+     * Creat a new bot with given ID and character
+     * 
+     * @param botID : the ID of the bot
+     * @param botCharacter : the charater of the bot
+     */
+    public Bot(int botID, char botCharacter)
+    {
+        super("BOT", "", 0, botID, botCharacter, "");
+    }
+
+
+    
+}
